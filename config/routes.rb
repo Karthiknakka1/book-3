4be1dc67a01c4f841774a11a332a7ec95fb1604f
Karthiknakka1 <Karthiknakka1@gmail.com>
2025-02-03 22:06:24 -0600

    completed book 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
   get 'main/index'
   resources :books do
     member do
       get :delete
     end
   end
-  root "books#index"
+  root "user_books#index"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
